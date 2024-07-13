a9f4cbc0821c47ddeb3ff4b6910a295a48e4b5ea
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-07-13 02:10:15 -0600

    Carga de productos desde Stripe

---
+++ js/stripe.js
@@ -0,0 +1,204 @@
+import stripe_keys from "./stripe_keys.js";
+
+const API_PRODUCTS = "https://api.stripe.com/v1/products";
+const API_PRICES = "https://api.stripe.com/v1/prices";
+const $temSwiperContainer = document.querySelector(
+  ".tem-swiper-container"
+).content;
+const $temSwiperSlide = document.querySelector(".tem-swiper-slide").content;
+const $fragment = document.createDocumentFragment();
+let products;
+let prices;
+let swipers;
+
+const fetchOptions = {
+  headers: {
+    Authorization: `Bearer ${stripe_keys.secret}`,
+  },
+};
+
+const categoryVideogames = (el, object, str) => {
+  if (el) {
+    let nameIndex = el.name.toLowerCase().indexOf(str);
+    let consoleName = el.name.slice(nameIndex + 1, el.name.length - 1);
+    let videogameName = el.name.slice(0, nameIndex - 1);
+    if (!object[consoleName]) object[consoleName] = {};
+    object[consoleName][videogameName] = el;
+  }
+};
+
+Promise.all([
+  fetch(API_PRODUCTS, fetchOptions),
+  fetch(API_PRICES, fetchOptions),
+])
+  .then((responses) =>
+    Promise.all(
+      responses.map((res) => (res.ok ? res.json() : Promise.reject(res)))
+    )
+  )
+  .then((json) => {
+    products = json[0].data;
+    prices = json[1].data;
+    let playstationObject = {};
+    let xboxObject = {};
+    prices.forEach((el) => {
+      let response = products.filter((product) => product.id === el.product);
+      let xbox = response.filter(
+        (res) =>
+          res.name.toLowerCase().search(/(\(xbox\s+[0-9a-z]+)|\(xbox/gi) !== -1
+      );
+      let playstation = response.filter(
+        (res) => res.name.toLowerCase().search(/\(ps[0-9 || a-z]+/gi) !== -1
+      );
+      playstation.forEach((el) =>
+        categoryVideogames(el, playstationObject, "(ps")
+      );
+      xbox.forEach((el) => categoryVideogames(el, xboxObject, "(xbox"));
+    });
+
+    for (let platform in playstationObject) {
+      let $cloneSlide;
+      let $clonePlatform = document.importNode($temSwiperContainer, true);
+      for (let videogame in playstationObject[platform]) {
+        $cloneSlide = document.importNode($temSwiperSlide, true);
+        let $videogame = $cloneSlide.querySelector(".videogame");
+        $videogame.dataset.front = `./assets/img/playstation/${platform.toLowerCase()}/${videogame
+          .toLowerCase()
+          .trim()}/front.jpg`;
+        $videogame.dataset.back = `./assets/img/playstation/${platform.toLowerCase()}/${videogame
+          .toLowerCase()
+          .trim()}/back.jpg`;
+        $videogame.dataset.side = `./assets/img/playstation/${platform.toLowerCase()}/${videogame
+          .toLowerCase()
+          .trim()}/side.jpg`;
+        $clonePlatform
+          .querySelector(".swiper-wrapper")
+          .appendChild($cloneSlide);
+      }
+      $fragment.appendChild($clonePlatform);
+    }
+
+    document.querySelector("main").appendChild($fragment);
+
+    const videogames = document.querySelectorAll(".videogame");
+
+    let interval;
+    let deg = 0;
+
+    function mouseOverVideogame(videogame) {
+      if (!interval) {
+        interval = setInterval(() => {
+          deg = (deg + 1) % 360;
+          videogame.style.transform = `rotateY(${deg}deg)`;
+        }, 30);
+      }
+    }
+
+    function mouseLeaveVideogame(videogame) {
+      clearInterval(interval);
+      interval = null;
+      deg = 0;
+      videogame.style.transform = "rotateY(0deg)";
+    }
+
+    videogames.forEach((videogame) => {
+      let $front = videogame.querySelector(".front");
+      let $back = videogame.querySelector(".back");
+      let $side = videogame.querySelector(".side");
+      $front.style.background = `url("${videogame.dataset.front}")`;
+      $front.style.backgroundSize = "cover";
+      $back.style.background = `url("${videogame.dataset.back}")`;
+      $back.style.backgroundSize = "cover";
+      $side.style.background = `url("${videogame.dataset.side}")`;
+      $side.style.backgroundSize = "cover";
+
+      // videogame.addEventListener("mouseover", function (e) {
+      //   if (!interval) {
+      //     interval = setInterval(() => {
+      //       deg = (deg + 1) % 360;
+      //       videogame.style.transform = `rotateY(${deg}deg)`;
+      //     }, 30);
+      //   }
+      // });
+
+      // videogame.addEventListener("mouseleave", (e) => {
+      //   clearInterval(interval);
+      //   interval = null;
+      // });
+    });
+
+    swipers = new Swiper(".videogame-showcase", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        depth: 500,
+        modifer: 1,
+        slidesShadows: true,
+        rotate: 0,
+        stretch: 0,
+      },
+    });
+
+    //Creamos un manejador para hacer una misma referencia al listener
+    function createMouseOverHandler(videogame) {
+      return function () {
+        mouseOverVideogame(videogame);
+      };
+    }
+
+    function createMouseLeaveHandler(videogame) {
+      return function () {
+        mouseLeaveVideogame(videogame);
+      };
+    }
+
+    function videogamesSlide() {
+      swipers.forEach((swiper, i) => {
+        swiper.slides.forEach((slide, index) => {
+          let $videogame = slide.querySelector(".videogame");
+
+          // Remueve los eventos antes de añadirlos para evitar duplicados
+          if ($videogame.mouseOverHandler) {
+            $videogame.removeEventListener(
+              "mouseover",
+              $videogame.mouseOverHandler
+            );
+          }
+          if ($videogame.mouseLeaveHandler) {
+            $videogame.removeEventListener(
+              "mouseleave",
+              $videogame.mouseLeaveHandler
+            );
+          }
+
+          if (index === swiper.realIndex) {
+            // Solo el slide activo debería tener habilitado el efecto de girar
+            $videogame.mouseOverHandler = createMouseOverHandler($videogame);
+            $videogame.mouseLeaveHandler = createMouseLeaveHandler($videogame);
+            $videogame.addEventListener(
+              "mouseover",
+              $videogame.mouseOverHandler
+            );
+            $videogame.addEventListener(
+              "mouseleave",
+              $videogame.mouseLeaveHandler
+            );
+            //document.querySelectorAll(".btn-buy")[i].dataset.videogame =
+            //swiper.realIndex;
+          } else {
+            mouseLeaveVideogame($videogame);
+          }
+        });
+        swiper.on("slideChange", () => {
+          videogamesSlide();
+        });
+      });
+    }
+
+    videogamesSlide();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ js/stripe_keys.js
@@ -0,0 +1,6 @@
+export default {
+  secret:
+    "sk_test_51PbBxhBRlXe6jCgsy5BdAQ3P05lNbqZot960DN49fVqol2PhIz00QkeplqTXZ6xRCSxHz46JaTWX80cWOmrqJbRZ00uMLdh5XF",
+  public:
+    "pk_test_51PbBxhBRlXe6jCgsnbYHIZQudHz1cQEWuLD45klEjWEAbjKU6gA1UNOjWwRmzSUasNKOJzWQuHUlWCcdyurBABhT00l8Vbue4U",
+};
